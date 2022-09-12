77bdb1e5875ff208af0a3ab043f3e10da9fd80c2
Diego Linares <diegojeanluck@hotmail.com>
2022-09-12 14:35:29 -0500

    Feat: mandatory task_0 and works

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Using a REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+    base_url = 'https://jsonplaceholder.typicode.com/users/'
+    user_id = argv[1]
+    final_msg = 'Employee {} is done with tasks({}/{}):'
+
+    user_url = '{}{}'.format(base_url, user_id)
+    todo_url = '{}{}/todos'.format(base_url, user_id)
+    completed_tasks_url = '{}?completed=true'.format(todo_url)
+
+    user = get(user_url).json()
+    all_tasks = get(todo_url).json()
+    completed_tasks = get(completed_tasks_url).json()
+
+    name = user.get('name')
+    total_tasks = len(all_tasks)
+    done_tasks = len(completed_tasks)
+
+    print(final_msg.format(name, done_tasks, total_tasks))
+
+    for task in completed_tasks:
+        print('\t{}'.format(task.get("title")))
