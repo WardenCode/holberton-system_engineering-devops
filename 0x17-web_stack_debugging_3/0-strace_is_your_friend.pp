091585239b853074f7f3bb2d7c95bdb1a3e844cb
Diego Linares <diegojeanluck@hotmail.com>
2022-09-28 17:30:13 -0500

    Feat: mandatory task #0 and works

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+exec {'fix typo phpp in configuration file':
+  command  => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
+  user     => 'root',
+  provider => 'shell',
+  path     => '/usr/local/bin/:/bin:/usr/sbin'
+}
