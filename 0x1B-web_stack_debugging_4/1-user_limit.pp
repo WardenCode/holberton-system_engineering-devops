ae463849ad6d70a8d9e61baa70d470b37bed0b0f
Diego Linares <diegojeanluck@hotmail.com>
2022-10-06 12:44:58 -0500

    Fix: typo simple quote and order of flags

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,7 @@
 # Fix files user limit
 
 exec { 'Cuantity of files that can open the user holberton':
-  comamnd  => 'sed -ir \'s/nofile [0-9]+$/nofile 5000/' /etc/security/limits.conf',
+  comamnd  => 'sed -ir \'s/nofile [0-9]+$/nofile 5000/\' /etc/security/limits.conf',
   user     => 'root',
   provider => 'shell',
   path     => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
